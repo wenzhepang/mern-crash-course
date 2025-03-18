bd513e45502c2da94652a7539def1624bbf5fefd
庞文哲 <pangwenzhe@pangwenzhedeMacBook-Air.local>
2025-03-18 22:47:15 +1300

    fix toast bug

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,58 +5,64 @@ import {
   SimpleGrid,
   VStack,
   Text,
-  Card,
-  CardBody,
-  Image,
+  Button,
+  Box,
 } from "@chakra-ui/react";
 import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
 import { toaster, Toaster } from "@/components/ui/toaster";
+
 const HomePage = () => {
   const { products, fetchProducts } = useProductStore();
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
 
-  
   return (
     <Container maxW={"container.sm"} py={12}>
-      <VStack spacing={8}>
-        <Text fontSize={"2xl"} fontWeight={"bold"}>
-          Home Page
-        </Text>
-        <SimpleGrid 
-          columns={{
-            base: 1,
-            md: 2,
-            lg: 3
-          }}
-          w={"full"}
-          spacing={10}
-          gap={10}
-        >
-          {Array.isArray(products) && products.length > 0 ? (
-            products.map((product) => (
-              <ProductCard key={product._id} product={product} />
-            ))
-          ) : (
-            <Text
-              fontSize={"2xl"}
-              fontWeight={"bold"}
-              _hover={{ color: "blue.500" }}
-            >
+      <Toaster />
+      {products.length === 0 ? (
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <VStack align="center">
+            <Text fontSize="2xl" fontWeight="bold" color="gray.600" cursor="default">
               No products found
             </Text>
-          )}
-        </SimpleGrid>
-
-        <Link to={"/create"}>
-          <Text as={"span"} color={"blue.500"} _hover={{ color: "blue.700" }}>
-            Create Product
-          </Text>
-        </Link>
-      </VStack>
-      <Toaster />
+            <Link to="/create">
+              <Button colorScheme="blue" size="md" variant="solid" 
+                _hover={{ 
+                  transform: "scale(1.05)",
+                  transition: "all 0.3s ease",
+                  filter: "brightness(0.8)"
+                }}
+                _active={{
+                  transform: "scale(0.8)",
+                  transition: "all 0.3s ease",
+                  bg: "gray.600"
+                }}
+              >
+                + Create Product
+              </Button>
+            </Link>
+          </VStack>
+        </Box>
+      ) : (
+        <VStack spacing={8}>
+          <SimpleGrid
+            columns={{
+              base: 1,
+              md: 2,
+              lg: 3,
+            }}
+            w={"full"}
+            spacing={10}
+            gap={10}
+          >
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </SimpleGrid>
+        </VStack>
+      )}
     </Container>
   );
 };
